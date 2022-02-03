cd3dc2554f6c93dd0937a8afe66cf7b32d91eea9
coreyshaver <cshaver2@ucmerced.edu>
2022-02-02 19:08:54 -0800

    Improving Homework

--- 207_HW1.foo.R
+++ 207_HW1.foo.R
@@ -5,26 +5,19 @@
 #' output: html_document
 ---
 
-#+ r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
+  #' ---
+  #' title: "R scripts can be rendered!"
+  #' output: github_document
+  #' ---
+  #'
+  #' Here's some prose in a very special comment. Let's summarize the built-in
+  #' dataset `VADeaths`.
+  # here is a regular code comment, that will remain as such
+  summary(VADeaths)
 
-
-## R Markdown
-
-#This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
-
-#When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
-
-#+ r cars}
-summary(cars)
-
-
-## Including Plots
-
-#You can also embed plots, for example:
-
-#+ r pressure, echo=FALSE}
-plot(pressure)
-
-
-#Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
+#' Here's some more prose. I can use usual markdown syntax to make things
+#' **bold** or *italics*. Let's use an example from the `dotchart()` help to
+#' make a Cleveland dot plot from the `VADeaths` data. I even bother to name
+#' this chunk, so the resulting PNG has a decent name.
+#+ dotchart
+dotchart(VADeaths, main = "Death Rates in Virginia - 1940")
